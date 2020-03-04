1de7fd9b36db1b6acee7d8ca555f2486a5824ba4
winton <mail@wintoni.us>
2020-03-04 13:59:19 -0800

    Add BootstrapComponent and StackComponent to stack

--- tsignore/stack.ts
+++ tsignore/stack.ts
@@ -10,8 +10,10 @@ import { Ssr } from "../ssr"
 import { TinyId } from "../tinyId"
 
 import { App } from "./app"
+import { BootstrapComponent } from "./components/bootstrapComponent"
 import { HeadComponent } from "./components/headComponent"
 import { HomeComponent } from "./components/homeComponent"
+import { StackComponent } from "./components/stackComponent"
 
 export default ():
   | Record<string, any>
@@ -29,7 +31,9 @@ export default ():
     // app
     app: new App(),
     // components
+    bootstrapComponent: new BootstrapComponent(),
     headComponent: new HeadComponent(),
     homeComponent: new HomeComponent(),
+    stackComponent: new StackComponent(),
   })
 }
